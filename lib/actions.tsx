37799451f45ad1be3444580c99ee0bd5a655fe34
David Cramer <dcramer@gmail.com>
2025-01-15 13:06:45 -0800

    More sentry calls to aid debugging

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -1,5 +1,9 @@
+import * as Sentry from "@sentry/nextjs";
+
 export function action() {
   "use action";
+
+  Sentry.captureMessage("server action firing");
 
   throw new Error("Error in server action");
 }
@@ -7,13 +11,19 @@ export function action() {
 export async function asyncAction() {
   "use action";
 
+  Sentry.captureMessage("server async action firing");
+
   throw new Error("Error in async server action");
 }
 
 export function nonAction() {
+  Sentry.captureMessage("client handler firing");
+
   throw new Error("Error in client handler");
 }
 
 export async function asyncNonAction() {
+  Sentry.captureMessage("async client handler firing");
+
   throw new Error("Error in async client handler");
 }
